b9a0b18cec622ab103449877d996920e3c4023b4
DerpyTBPz <dima.plyasov13@gmail.com>
2022-01-31 10:46:11 +0200

    com

--- code.cpp
+++ code.cpp
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <clocale>
 
-int main() // Шифрование да
+int main() // Шифрование да lf
 { 
     char abc[] = " abcdefghigklmnopqrstuvwxyz";
     int textNumb[200], codeNumb[50], k = 0, codeTextNumb[200], codeSize = 0, textSize = 0;
